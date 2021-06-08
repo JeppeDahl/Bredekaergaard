4e88cfa71f9398eb3b7bcd32dec8d92dbd8bce5c
JeppeDahl <85563093+JeppeDahl@users.noreply.github.com>
2021-06-08 15:12:40 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,170 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_0257_nysky2",
+      "name": "DJI_0257_NYSKY2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.4597240875452009,
+        "pitch": 0.12004408274192713,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6602107919035269,
+          "pitch": -0.4327192090799272,
+          "rotation": 3.141592653589793,
+          "target": "3-dji_0255"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8928297658323991,
+          "pitch": 0.10817559647151143,
+          "title": "Ny Legeplads opført 2021",
+          "text": "Her bloiver lavet en legeplads"
+        }
+      ]
+    },
+    {
+      "id": "1-0",
+      "name": "0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dji_0254",
+      "name": "DJI_0254",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.102708457176668,
+          "pitch": -0.1264358764954796,
+          "rotation": 0,
+          "target": "3-dji_0255"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dji_0255",
+      "name": "DJI_0255",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0830422485188649,
+          "pitch": 0.26772089547709754,
+          "rotation": 0,
+          "target": "2-dji_0254"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.66412815176084,
+          "pitch": 0.6632096226409168,
+          "title": "kursuscenter",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "bredekærgaard",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
